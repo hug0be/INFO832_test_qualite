6af4bfb229b157d802fd029d6f1d58087e5e0196
ronanter <ronan1terras@gmail.com>
2024-03-12 16:40:47 +0100

    [TEST] -X for javadoc

--- src/main/java/timer/DateTimer.java
+++ src/main/java/timer/DateTimer.java
@@ -13,6 +13,7 @@ public class DateTimer  implements Timer {
 	 * Constructs a new DateTimer object.
 	 *
 	 * @param dates a set of dates
+	 * @see feur
 	 */
 	public DateTimer(TreeSet<Integer> dates) {
 		this.lapsTimes = new Vector<Integer>();
