d7e629c5fc597a315a2591487a09512fc78eee20
ValentinPB <122006974+ValentinPB@users.noreply.github.com>
2024-03-12 14:23:27 +0100

    Javadoc comments in DiscreteBehaviorSimulator
    added comments for the javadoc in the classes of the DiscreteBehaviorSimulator module.

--- src/main/java/discreteBehaviorSimulator/LogFormatter.java
+++ src/main/java/discreteBehaviorSimulator/LogFormatter.java
@@ -12,6 +12,12 @@ import java.util.logging.LogRecord;
  */
 public class LogFormatter  extends Formatter {
 	
+	/**
+	* This method is used to format a log record.
+	* 
+	* @param a LogRecord object
+	* @return the contents of the log record in type String
+	*/
 	public String format(LogRecord rec) {
 		StringBuffer buf = new StringBuffer();
 		
@@ -25,6 +31,12 @@ public class LogFormatter  extends Formatter {
 		return buf.toString();
 	}
 	
+	/**
+	* Calculates and formats the date.
+	* 
+	* @param milliseconds in type long
+	* @return the corresponding date in type String
+	*/
 	private String calcDate(long millisecs) {
 	    SimpleDateFormat date_format = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss.SS");
 	    Date resultdate = new Date(millisecs);
@@ -33,12 +45,24 @@ public class LogFormatter  extends Formatter {
 
 	  // this method is called just after the handler using this
 	  // formatter is created
+	  /**
+	  * This method is called just after the handler using this. formatter is created
+	  * 
+	  * @param a Handler object
+	  * @return an empty String
+	  */
 	  public String getHead(Handler h) {
 		  return "";
 	  }
 	  
 	// this method is called just after the handler using this
 	  // formatter is closed
+	  /**
+	  * This method is called jus after the handler using this. formatter is closed
+	  * 
+	  * @param a Handler object
+	  * @return an empty String
+	  */
 	  public String getTail(Handler h) {
 	    return "";
 	  }
