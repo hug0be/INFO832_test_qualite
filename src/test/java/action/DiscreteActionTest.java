4a03c3888698556877a04bca98375165654d226e
Coltarm <colin.treve@laposte.net>
2024-05-20 23:41:24 +0200

    fusion

--- src/test/java/action/DiscreteActionTest.java
+++ src/test/java/action/DiscreteActionTest.java
@@ -23,16 +23,45 @@ public class DiscreteActionTest {
     }
 
     @Test
-    void spendTime(){
-        discreteActionlittle.spendTime(0);
-        assertEquals(7, (int) discreteActionlittle.getCurrentLapsTime());
-        discreteActionlittle.spendTime(5);
-        assertEquals(2, (int) discreteActionlittle.getCurrentLapsTime());
+    void spendTimeLittleAction() {
+        try {
+            discreteActionlittle.spendTime(0);
+            assertEquals(7, (int) discreteActionlittle.getCurrentLapsTime());
+        } catch (NullPointerException e) {
+            fail();
+        }
+    }
+    @Test
+    void spendTimeLittleActionNoZerosValue() {
+        try {
+            discreteActionlittle.spendTime(5);
+            assertEquals(2, (int) discreteActionlittle.getCurrentLapsTime());
+        } catch (NullPointerException e) {
+            fail();
+        }
+    }
+
+    @Test
+    void spendTimeLittleActionNegativeValue() {
         assertThrowsExactly(IllegalArgumentException.class, () -> discreteActionlittle.spendTime(-10));
-        discreteActionBig.spendTime(2147483647);
-        assertEquals(0, (int) discreteActionBig.getCurrentLapsTime());
-        discreteActionMid.spendTime(100000);
-        assertEquals(0, (int) discreteActionMid.getCurrentLapsTime());
+    }
+
+    @Test
+    void spendTimeBigAction() {
+        try {
+            discreteActionBig.spendTime(2147483647);
+            assertEquals(0, (int) discreteActionBig.getCurrentLapsTime());
+        } catch (NullPointerException e) {
+            fail();
+        }
+    }
+
+    @Test
+    void spendTimeMidAction(){
+        try{
+            discreteActionMid.spendTime(100000);
+            assertEquals(0, (int) discreteActionMid.getCurrentLapsTime());
+        } catch (NullPointerException e){fail();}
     }
 
     @Test
